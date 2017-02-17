423d1ec3eba40ab8024d2cf30bba7cfe3f480517
daveaugerinos <dave.augerinos@gmail.com>
2017-02-17 11:28:41 -0800

    Initial build

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Dave Augerinos on 2017-02-17.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
